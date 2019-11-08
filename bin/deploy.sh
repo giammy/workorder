12a38757d81674d27ef8e5a9251bf3e729112ee8
Gianluca Moro <giangiammy@gmail.com>
2019-11-08 17:32:15 +0100

    export workslist to configurable locations

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,14 +9,20 @@ DEPLOY_HOST=`echo $DEPLOY_TO | cut -d':' -f1`
 
 cd ..
 rm -rf workorder/var/cache/*
-#rsync -a --delete --exclude var/data.db --exclude public/local workorder root@$DEPLOY_TO
-rsync -a --delete workorder root@$DEPLOY_TO
+
+# FULL COPY
+#rsync -a --delete workorder root@$DEPLOY_TO
+
+# CODE UPGRADE
+rsync -a --delete --exclude var/data.db --exclude public/local workorder root@$DEPLOY_TO
+if [ x$1 = "xGET" ]; then
+  rsync -a root@${DEPLOY_TO}/workorder/var/data.db workorder/var
+  rsync -a root@${DEPLOY_TO}/workorder/public/local workorder/public
+fi
+
 ssh root@$DEPLOY_HOST "echo 'APP_ENV=prod' >> /var/www/html/workorder/.env.local"
 ssh root@$DEPLOY_HOST "chown -R apache:apache /var/www/html/workorder"
 
-#scp root@${DEPLOY_TO}/staff/var/data.db staff/var
-#scp -r root@${DEPLOY_TO}/staff/public/local staff/public
 
-cd staff
-
+cd workorder
 
--- src/Services/ExportWorkslistService.php
+++ src/Services/ExportWorkslistService.php
@@ -22,11 +22,12 @@ class ExportWorkslistService {
     
     public function export($filenamePar) {
         $filename = $filenamePar?$filenamePar:$this->params->get('export_workslist_filename');
+        $filenameBis = $filenamePar?$filenamePar:$this->params->get('export_workslist_filename_bis');
         // var_dump($filename);exit;
 
         $this->appLogger->info("IN: ExportWorkslistService.export: filename=" . $filename);
 
-        file_put_contents($filename, "CODE,Task/Activity,Contract ref ,RESPONSIBLE&CONTACT_PERSON,start ,end\n");
+        file_put_contents($filename,    "CODE,Task/Activity,Contract ref ,RESPONSIBLE&CONTACT_PERSON,start ,end\n");
 
         $repo = $this->manager->getRepository(Workslist::class);
         $listToShow = $repo->findAll();
@@ -34,14 +35,16 @@ class ExportWorkslistService {
         foreach ($listToShow as $x) {
             $ostr = "";
             $ostr = $ostr . $x->getActivityCodePrefix() . "-" . $x->getActivityCodeSuffix() . ",";
-            $ostr = $ostr . $x->getDescription() . ",";
+            $ostr = $ostr . "\"" . $x->getDescription() . "\",";
             $ostr = $ostr . $x->getWorkorder() . ",";
             $ostr = $ostr . $x->getResponsible() . "&" . $x->getDeputy() . ",";
-            $ostr = $ostr . $x->getValidFrom()->format("Y") . ",";
-            $ostr = $ostr . $x->getValidTo()->format("Y");
+            $ostr = $ostr . $x->getValidFrom()->format("n") . ","; // print month
+            $ostr = $ostr . $x->getValidTo()->format("n");
 
-            file_put_contents($filename, $ostr . "\n", FILE_APPEND);
+            file_put_contents($filename,    $ostr . "\n", FILE_APPEND);
         }
+        unlink($filenameBis);
+        copy($filename, $filenameBis);
     }
 }
 
